1e8e76d9b7d8a937a57a6d52c46dddf75bd3b840
Simon Bausch <simon@bausch-alfdorf.de>
2018-06-04 09:21:34 +0200

    case insensitive constant names in EnumPropertyTypeMapper

--- src/org/unclesniper/choreo/EnumPropertyTypeMapper.java
+++ src/org/unclesniper/choreo/EnumPropertyTypeMapper.java
@@ -1,8 +1,13 @@
 package org.unclesniper.choreo;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class EnumPropertyTypeMapper implements PropertyTypeMapper {
 
 	public static final EnumPropertyTypeMapper instance = new EnumPropertyTypeMapper();
+
+	private final Map<Class<?>, Boolean> allCapital = new HashMap<Class<?>, Boolean>();
 
 	public EnumPropertyTypeMapper() {}
 
@@ -14,7 +19,27 @@ public class EnumPropertyTypeMapper implements PropertyTypeMapper {
 	public Object mapPropertyValue(BuildContext context, Object value, Class<?> toType) {
 		if(value == null)
 			throw new IllegalArgumentException("Enum constant name cannot be null");
-		return Enum.valueOf((Class)toType, (String)value);
+		String name = (String)value;
+		Boolean cap = allCapital.get(toType);
+		if(cap == null) {
+			cap = EnumPropertyTypeMapper.areAllCapital((Class)toType);
+			allCapital.put(toType, cap);
+		}
+		if(cap)
+			name = name.toUpperCase();
+		return Enum.valueOf((Class)toType, name);
+	}
+
+	private static boolean areAllCapital(Class<? extends Enum<?>> clazz) {
+		Enum<?>[] constants = clazz.getEnumConstants();
+		if(constants == null)
+			return false;
+		for(Enum<?> constant : constants) {
+			String name = constant.name();
+			if(!name.equals(name.toUpperCase()))
+				return false;
+		}
+		return true;
 	}
 
 }
